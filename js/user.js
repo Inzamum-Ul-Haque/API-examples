bd81d0eeabbdc96062ca156401bc16bc81db2b9d
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-08-29 01:39:57 +0600

    Saving

---
+++ js/user.js
@@ -0,0 +1,18 @@
+const loadUsers = () => {
+  const url = "https://randomuser.me/api/?gender=female";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.results[0]))
+    .catch((error) => console.log(error));
+};
+
+const loadUserAsync = async () => {
+  const url = "https://randomuser.me/api/?gender=female";
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
